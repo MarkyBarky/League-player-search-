3b28d8c94f9ca6046b678800067b4b16f4adfc15
Mark Kim <your>
2022-07-26 22:36:56 +1200

    added player icon and material ui search bar

--- leaguesearch/src/App.tsx
+++ leaguesearch/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import axios from "axios";
 import logo from './logo.svg';
-import './App.css';
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import "./App.css";
+
 
 
 
 function App() {
+
   const [searchText, setSearchText] = useState("");
   const [playerData, setPlayerData] = useState ({});
   const API_KEY = "RGAPI-caca577c-4b61-403a-9c9c-44e1ec691bfd";
@@ -28,9 +32,19 @@ function App() {
     <div className="App">
       <div className="Container">
         <h5>Mark's League of Legends player search</h5>
-        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
-        <button onClick={e => searchForPlayer()}>search for player</button>
+        <input type="text" placeholder= "search..." onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForPlayer()}><SearchIcon style={{ fill: "blue" }} />search for player</button>
       </div>
+      {JSON.stringify(playerData) !== "{}" ? 
+      <>
+      <p>{playerData.name}</p>
+      <img width="150" height="150" alt="summoner icon" src={"http://ddragon.leagueoflegends.com/cdn/12.13.1/img/profileicon/" + playerData.profileIconId + ".png"}></img>
+      <p>Summoner level {playerData.summonerLevel}</p>
+      </>
+      :
+      <><p>No player data</p></>
+      
+      }
     </div>
   );
 }
